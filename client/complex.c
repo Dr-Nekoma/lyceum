f94f6171368938f08c0d94fbfe84c21239582fee
EduardoLR10 <dudulr10@gmail.com>
2024-04-08 22:44:00 -0300

    Add draft of ports

---
+++ client/complex.c
@@ -0,0 +1,7 @@
+int foo(int x) {
+  return x*5;
+}
+
+int bar(int y) {
+  return y*2;
+}
--- src/server.erl
+++ src/server.erl
@@ -5,9 +5,10 @@
 
 -module(server).
 
--behaviour(application).
+%-behaviour(application).
 
--export([start/2, start/0, stop/1]).
+-export([start/1, init/1, start/2, start/0, stop/1, stop/0]).
+-export([foo/1, bar/1]).
 
 start(_StartType, _StartArgs) ->
     Dispatch = cowboy_router:compile([{'_', [{"/", hello_handler, []},
@@ -21,3 +22,50 @@ start() ->
 
 stop(_State) ->
     ok.
+
+start(ExtPrg) ->
+    spawn(?MODULE, init, [ExtPrg]).
+stop() ->
+    complex ! stop.
+
+foo(X) ->
+    call_port({foo, X}).
+bar(Y) ->
+    call_port({bar, Y}).
+
+call_port(Msg) ->
+    complex ! {call, self(), Msg},
+    receive
+	{complex, Result} ->
+	    Result
+    end.
+
+init(ExtPrg) ->
+    register(complex, self()),
+    process_flag(trap_exit, true),
+    Port = open_port({spawn, ExtPrg}, [{packet, 2}, binary]),
+    loop(Port).
+
+loop(Port) ->
+    receive
+	{call, Caller, Msg} ->
+	    Port ! {self(), {command, term_to_binary(Msg)}},
+	    receive
+		{Port, {data, Data}} ->
+		    Caller ! {complex, binary_to_term(Data)}
+	    end,
+	    loop(Port);
+	stop ->
+	    Port ! {self(), close},
+	    receive
+		{Port, closed} ->
+		    exit(normal)
+	    end;
+	{'EXIT', Port, Reason} ->
+	    exit(port_terminated)
+    end.
+
+encode({foo, X}) -> [1, X];
+encode({bar, Y}) -> [2, Y].
+
+decode([Int]) -> Int.
