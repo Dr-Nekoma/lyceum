d97ce20ba45bff05cc8d0833416b8877224665d3
EduardoLR10 <dudulr10@gmail.com>
2024-04-15 22:09:14 -0300

    Add draft of zig client

---
+++ client/myprog.zig
@@ -0,0 +1,52 @@
+const ei = @cImport(
+    @cInclude("ei.h"),
+);
+
+const std = @import("std");
+const server_name = "lyceum_server@nixos";
+
+const LNode = struct {
+    c_node: ei.ei_cnode,
+    fd: i32,
+    node_name: [*c]const u8 = "lyceum_client@nixos",
+    port: i32 = 4369,
+    identification_number: i32 = 99,
+    cookie: [*c]const u8 = "lyceum",
+};
+
+pub fn prepare_connection() !LNode {
+    var l_node: LNode = .{
+        .c_node = undefined,
+        .fd = undefined,
+    };
+    const creation = std.time.timestamp() + 1;
+    const creation_u: u64 = @bitCast(creation);
+    const creation_c: c_uint = @truncate(creation_u);
+    const result = ei.ei_connect_init(&l_node.c_node,
+                                      l_node.node_name,
+                                      l_node.cookie,
+                                      creation_c);
+    if (result < 0) return error.ei_connect_init_failed;
+    return l_node;
+}
+
+pub fn establish_connection(ec: *LNode) !void {
+    const sockfd: i32 = ei.ei_connect_host_port(&ec.c_node, @constCast(server_name), ec.port);
+    if (sockfd < 0) return error.ei_connect_failed;
+    ec.fd = sockfd;
+}
+
+pub fn send_message(ec: *LNode, message: [*c]const u8) void {
+    var buf: ei.ei_x_buff = undefined;
+    _ = ei.ei_x_new_with_version(&buf);
+    _ = ei.ei_x_encode_tuple_header(&buf, 2);
+    _ = ei.ei_x_encode_pid(&buf, ei.ei_self(&ec.c_node));
+    _ = ei.ei_x_encode_atom(&buf, message);
+    _ = ei.ei_reg_send(&ec.c_node, ec.fd, @constCast(server_name), buf.buff, buf.index);
+}
+
+pub fn main() !void {
+    var node: LNode = try prepare_connection();
+    try establish_connection(&node);    
+    send_message(&node, "Hello from Lyceum!");
+}
