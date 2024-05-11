b0f07f71ccd3843c87b6a3098efac4e6d6a3218d
EduardoLR10 <dudulr10@gmail.com>
2024-05-10 23:51:49 -0300

    Add draft of main menu scene

--- client/src/client.zig
+++ client/src/client.zig
@@ -1,50 +1,105 @@
 const erl = @import("erlang.zig");
 const std = @import("std");
-// const rl = @import("raylib");
+const rl = @import("raylib");
 
-pub fn main() !void {
-    const connection_status = erl.ei.ei_init();
-    if (connection_status != 0) return error.ei_init_failed;
-    var node: erl.LNode = try erl.prepare_connection();
-    try erl.establish_connection(&node);
-    try erl.send_payload(&node, .{
-        .user_registry = .{
-            .username = "Nathan",
-            .email = "zcxv@bnm.drn",
-            .password = "3756849034567890",
-        },
-    });
-    const msg: []const u8 = try erl.receive_message(&node);
-    std.debug.print("{s}", .{msg});
+// pub fn main() !void {
+//     const connection_status = erl.ei.ei_init();
+//     if (connection_status != 0) return error.ei_init_failed;
+//     var node: erl.LNode = try erl.prepare_connection();
+//     try erl.establish_connection(&node);
+//     try erl.send_payload(&node, .{
+//         .user_registry = .{
+//             .username = "Nathan",
+//             .email = "zcxv@bnm.drn",
+//             .password = "3756849034567890",
+//         },
+//     });
+//     const msg: []const u8 = try erl.receive_message(&node);
+//     std.debug.print("{s}", .{msg});
+// }
+
+const fontSize = 20;
+pub const ColorPalette = struct {
+    pub const primary = rl.Color.init(13, 25, 23, 255);
+    pub const secondary = rl.Color.init(222, 222, 222, 255);
+    pub const background = rl.Color.init(68, 105, 129, 255);
+};
+
+pub fn drawButton(message: [:0]const u8, position: rl.Vector2, size: rl.Vector2, color: rl.Color) !void {
+    rl.drawRectangleV(position, size, color);
+    const messageSize: f32 = @floatFromInt(rl.measureText(message, fontSize));
+    const messageX = position.x + size.x / 2 - messageSize / 2;
+    const floatFont: f32 = @floatFromInt(fontSize);
+    const messageY = position.y + size.y / 2 - floatFont / 2;
+    rl.drawText(message, @intFromFloat(messageX), @intFromFloat(messageY), fontSize, ColorPalette.secondary);
 }
 
-// pub fn main() anyerror!void {
-//     // Initialization
-//     //--------------------------------------------------------------------------------------
-//     const screenWidth = 800;
-//     const screenHeight = 450;
+pub const GameState = struct {
+    sceneState: enum {
+        user_registry,
+        nothing,        
+    },
+    width: f32,
+    height: f32,
+    buttons: buttonMapType,
+    const buttonMapType = std.hash_map.AutoHashMap(rl.Vector4, AvailableButtons);
+    pub const AvailableButtons = enum {
+        user_registry
+    };
 
-//     rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
-//     defer rl.closeWindow(); // Close window and OpenGL context
+    pub fn init(allocator: std.mem.Allocator, width: f32, height: f32) !GameState {
+        if (width < 0) {
+            return error.negative_width;
+        }
+        if (height < 0) {
+            return error.negative_height;
+        }
+        return .{
+            .sceneState = .nothing,
+            .width = width,
+            .height = height,
+            .buttons = buttonMapType.init(allocator),
+        };
+    }
 
-//     rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
-//     //--------------------------------------------------------------------------------------
+    pub fn deinit(gameState: * @This()) void {
+        gameState.buttons.deinit();
+    } 
+        
+    pub fn drawMainMenu(gameState: *const @This()) anyerror!void {
+        const userCreationButtonHeight: f32 = gameState.height / 8;
+        const userCreationButtonWidth: f32 = gameState.width / 4;
+        try drawButton("Create User",
+                       .{.x = (gameState.width / 2) - (userCreationButtonWidth / 2),
+                         .y = (gameState.height / 2) - (userCreationButtonHeight / 2)},
+                       .{.x = userCreationButtonWidth,
+                         .y = userCreationButtonHeight},
+                       ColorPalette.primary);
+        // TODO: Judge whether we should do Raylib-way for effect-detection, i.e., you draw and check together, or the pure way, i.e., you do logic and effects separately.
+    }
 
-//     // Main game loop
-//     while (!rl.windowShouldClose()) { // Detect window close button or ESC key
-//         // Update
-//         //----------------------------------------------------------------------------------
-//         // TODO: Update your variables here
-//         //----------------------------------------------------------------------------------
+    pub fn drawCreateUser(gameState: *const @This()) anyerror!void {
+        rl.drawText("Congrats! You created your first window!", @intFromFloat(gameState.width / 4), @intFromFloat(gameState.height / 2), fontSize, rl.Color.black);
+    }
+};
 
-//         // Draw
-//         //----------------------------------------------------------------------------------
-//         rl.beginDrawing();
-//         defer rl.endDrawing();
+pub fn main() anyerror!void {
+    var gameState = try GameState.init(std.heap.c_allocator, 800, 450);
+    defer gameState.deinit();
 
-//         rl.clearBackground(rl.Color.white);
+    rl.setConfigFlags(.flag_window_resizable);
+    rl.initWindow(@intFromFloat(gameState.width), @intFromFloat(gameState.height), "Lyceum");   
+    defer rl.closeWindow();
+    rl.setTargetFPS(60);
+    
+    while (!rl.windowShouldClose()) {
+        rl.beginDrawing();
+        defer rl.endDrawing();
 
-//         rl.drawText("Congrats! You created your first window!", 190, 200, 20, rl.Color.light_gray);
-//         //----------------------------------------------------------------------------------
-//     }
-// }
+        rl.clearBackground(ColorPalette.background);
+        gameState.width = @floatFromInt(rl.getScreenWidth());
+        gameState.height = @floatFromInt(rl.getScreenHeight());
+
+        try GameState.drawMainMenu(&gameState);       
+    }   
+}
