a0915327d659186c0618047bb19766756c98abe1
Marcos Magueta <maguetamarcos@gmail.com>
2024-06-03 23:07:37 -0300

    add trigger for checking overlaps

--- database/main.sql
+++ database/main.sql
@@ -12,15 +12,46 @@ CREATE TABLE "map"(
        PRIMARY KEY("name")
 );
 
+CREATE TYPE TILE_TYPE AS ENUM(
+       'WATER',
+       'GRASS',
+       'SAND',
+       'ROCK'
+);
+
+CREATE TABLE "tile"(
+       "map_name" VARCHAR(16) NOT NULL, 
+       "kind" TILE_TYPE NOT NULL,
+       "x_position" SMALLINT NOT NULL,
+       "y_position" SMALLINT NOT NULL,
+       PRIMARY KEY("map_name", "kind", "x_position", "y_position"),
+       FOREIGN KEY ("map_name") REFERENCES "map"("name")
+);
+
 CREATE TABLE "object"(
        "map_name" VARCHAR(16) NOT NULL, 
        "name" VARCHAR(16) NOT NULL,
        "x_position" SMALLINT NOT NULL,
        "y_position" SMALLINT NOT NULL,
-       "z_position" SMALLINT NOT NULL,       
-       FOREIGN KEY ("map_name") REFERENCES "map"("name"),	
-       PRIMARY KEY("map_name")
+       PRIMARY KEY("map_name", "x_position", "y_position"),
+       FOREIGN KEY ("map_name") REFERENCES "map"("name")
 );
+
+CREATE OR REPLACE FUNCTION map_object_overlap() RETURNS trigger AS $map_object_overlap$
+       DECLARE
+	garbage FOR SELECT "kind" FROM "tile" WHERE "x_position" = NEW."x_position" AND "y_position" = NEW."y_position"
+       BEGIN
+	IF NEW."name" = 'TREE' THEN
+	   CASE WHEN garbage = 'WATER' OR garbage = 'ROCK' THEN RAISE EXCEPTION '''TREE'' cannot be defined in tiles that are not ''GRASS'' or ''SAND''.';
+	   ELSE RETURN NEW;
+	   END;
+	END IF;
+	RETURN NEW;
+       END;
+$map_object_overlap$ LANGUAGE plpgsql;
+
+CREATE TRIGGER map_object_overlap BEFORE INSERT OR UPDATE ON "object"
+FOR EACH ROW EXECUTE FUNCTION map_object_overlap();
 
 CREATE TABLE "character"(
        "name" VARCHAR(18) NOT NULL,
