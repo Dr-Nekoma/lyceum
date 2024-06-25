dfd9dd697958f08f5c0de81558ab958306090728
Marcos Magueta <maguetamarcos@gmail.com>
2024-06-24 22:08:53 -0300

    add listing of user chars

--- src/character.erl
+++ src/character.erl
@@ -1,6 +1,6 @@
 -module(character).
 
--export([create/2]).
+-export([create/2, player_characters/2]).
 
 create(#{name := Name, 
 	 username := Username, 
@@ -11,9 +11,14 @@ create(#{name := Name,
 	 endurance := Endurance,
 	 intelligence := Intelligence,
 	 faith := Faith}, Connection) ->
-    Query = "INSERT INTO lyceum.\"view_character\" (\"name\",  \"e-mail\", \"username\", \"constitution\", \"wisdom\", \"strength\", \"endurance\", \"intelligence\", \"faith\") \
+    Query = "INSERT INTO lyceum.view_character (\"name\",  \"e-mail\", \"username\", \"constitution\", \"wisdom\", \"strength\", \"endurance\", \"intelligence\", \"faith\") \
              VALUES ($1::VARCHAR(18), $2::TEXT, $3::VARCHAR(32), $4::SMALLINT, $5::SMALLINT, $6::SMALLINT, $7::SMALLINT, $8::SMALLINT, $9::SMALLINT)",
     {ok, _, _} = epgsql:equery(Connection, Query, [Name, Username, Email, Constitution, Wisdom, Strength, Endurance, Intelligence, Faith]).
+
+player_characters(#{username := Username,
+		    email := Email}, Connection) ->
+    Query = "SELECT * FROM lyceum.view_character WHERE username = $1::VARCHAR(32) AND e_mail = $2::TEXT",
+    {ok, _, _} = epgsql:equery(Connection, Query, [Username, Email]).
 
 
 %% INSERT INTO lyceum."view_character"("name", "e-mail", "username", "constitution", "wisdom", "strength", "endurance", "intelligence", "faith")
