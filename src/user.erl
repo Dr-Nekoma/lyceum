f9e86e0994713c5baa529789e480dacb7a9f9c25
EduardoLR10 <dudulr10@gmail.com>
2024-07-01 23:00:11 -0300

    Fix server to remove double quotes in erlang queries

--- src/user.erl
+++ src/user.erl
@@ -3,9 +3,9 @@
 -export([insert_user/2, check_user/2]).
 
 check_user(#{username := Username, password := Password}, Connection) ->
-    Query = "SELECT * FROM \"user\" WHERE \"username\" = $1::VARCHAR(32) AND \"password\" = $2::TEXT",
+    Query = "SELECT * FROM user WHERE username = $1::VARCHAR(32) AND password = $2::TEXT",
     {ok, _, _} = epgsql:equery(Connection, Query, [Username, Password]).
 
 insert_user(#{username := Username, email := Email, password := Password}, Connection) ->
-    Query = "INSERT INTO \"user\"  (\"username\", \"e-mail\", \"password\") VALUES ($1::VARCHAR(32), $2::TEXT, $3::TEXT)",
+    Query = "INSERT INTO user  (username, e-mail, password) VALUES ($1::VARCHAR(32), $2::TEXT, $3::TEXT)",
     {ok, _, _} = epgsql:equery(Connection, Query, [Username, Email, Password]).
