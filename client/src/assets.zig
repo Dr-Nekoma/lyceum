f5ef3c8cdec385d66db5919d2f7ffe6aede872b3
EduardoLR10 <dudulr10@gmail.com>
2024-09-01 03:39:01 -0300

    Add animations asset loader function

--- client/src/assets.zig
+++ client/src/assets.zig
@@ -29,7 +29,7 @@ pub fn model(modelFilePath: [:0]const u8) !rl.Model {
     const allocator: std.mem.Allocator = std.heap.c_allocator;
     const fullFilePath = try std.fs.path.joinZ(allocator, &.{ base_filepath, modelFilePath });
     defer allocator.free(fullFilePath);
-    if (checkExtension(modelFilePath, &.{ ".glb", ".obj" })) {
+    if (checkExtension(modelFilePath, &.{ ".glb", ".obj", ".m3d" })) {
         return rl.loadModel(fullFilePath);
     } else {
         std.debug.print("Error trying to load model: .{s}", .{fullFilePath});
@@ -46,5 +46,17 @@ pub fn texture(textureFilePath: [:0]const u8) !rl.Texture2D {
     } else {
         std.debug.print("Error trying to load texture: .{s}", .{fullFilePath});
         return error.could_not_load_texture;
+    }
+}
+
+pub fn animations(animationFilePath: [:0]const u8) ![]rl.ModelAnimation {
+    const allocator: std.mem.Allocator = std.heap.c_allocator;
+    const fullFilePath = try std.fs.path.joinZ(allocator, &.{ base_filepath, animationFilePath });
+    defer allocator.free(fullFilePath);
+    if (checkExtension(animationFilePath, &.{".m3d"})) {
+        return try rl.loadModelAnimations(fullFilePath);
+    } else {
+        std.debug.print("Error trying to load animation model: .{s}", .{fullFilePath});
+        return error.could_not_load_animation;
     }
 }
