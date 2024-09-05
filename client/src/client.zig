8dbe32fe30ac31232339410f6cd93ba2e60eea61
Eduardo Lemos Rocha <dudulr10@gmail.com>
2024-09-05 00:56:14 -0300

    First multiplayer (#16)
    * Add draft of new connection schema for multiplayer approach
    
    * Finish first draft of sending other players in the current session
    
    * Use the spin-off Zerl library
    
    * Add license to `build.zig.zon`
    
    * Pin a specific commit of raylib-zig
    
    * Make floor level lower
    
    * Update input and main schema files
    
    * Update communications when in spawn scene
    
    * Update communication and controls for LAN multiplayer
    
    * Make the code compile
    
    * Refactor major structs in client
    
    * Finish integrating Zerl into Lyceum
    
    * Reorder imports in all zig files
    
    * Fix typos
    
    * Add review comments
    
    ---------
    
    Co-authored-by: Marinho <marinho.pinotti@gmail.com>

--- client/src/client.zig
+++ client/src/client.zig
@@ -1,38 +1,32 @@
-const erl = @import("erlang.zig");
-const std = @import("std");
-const rl = @import("raylib");
-const config = @import("config.zig");
-const state = @import("game/state.zig");
-const user = @import("menu/user.zig");
+const assets = @import("assets.zig");
 const character = @import("menu/character.zig");
-const mainMenu = @import("menu/main.zig");
+const config = @import("config.zig");
+const connection = @import("menu/connection.zig");
 const game = @import("game/main.zig");
-
-pub fn print_connect_server_error(message: anytype) !void {
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print(
-        "Could not connect to Lyceum Server!\n\u{1b}[31mError: \u{1b}[37m{}\n",
-        .{message},
-    );
-}
+const mainMenu = @import("menu/main.zig");
+const rl = @import("raylib");
+const state = @import("game/state.zig");
+const std = @import("std");
+const user = @import("menu/user.zig");
+const zerl = @import("zerl");
 
 pub fn main() anyerror!void {
-    const connection_status = erl.ei.ei_init();
-    if (connection_status != 0) return error.ei_init_failed;
-    var node: erl.Node = try erl.prepare_connection();
-    erl.establish_connection(&node) catch |error_value| {
-        try print_connect_server_error(error_value);
-        std.process.exit(2);
-    };
-
-    var gameState = try state.init(800, 450, &node);
-    gameState.menu = .{ .character_name = try gameState.allocator.allocSentinel(u8, config.nameSize, 0) };
-    @memset(gameState.menu.character_name, 0);
-
     rl.setConfigFlags(.{ .window_resizable = true });
-    rl.initWindow(@intFromFloat(gameState.width), @intFromFloat(gameState.height), "Lyceum");
+    rl.initWindow(@intFromFloat(config.Screen.initialWidth), @intFromFloat(config.Screen.initialHeight), "Lyceum");
     defer rl.closeWindow();
     rl.setTargetFPS(60);
+    var node = try zerl.Node.init("lyceum");
+
+    var gameState = try state.init(config.Screen.initialWidth, config.Screen.initialHeight, &node);
+    gameState.menu = .{
+        .character = .{
+            .create = .{
+                .name = try gameState.allocator.allocSentinel(u8, config.nameSize, 0),
+            },
+        },
+        .assets = try mainMenu.loadAssets(),
+    };
+    @memset(gameState.menu.character.create.name, 0);
 
     // try character.goToSpawn(&gameState);
     mainMenu.spawn(&gameState);
@@ -61,9 +55,13 @@ pub fn main() anyerror!void {
             .character_selection => {
                 try character.selection(&gameState);
             },
+            .connect => {
+                try connection.connect(&gameState);
+            },
             .nothing => {
                 mainMenu.spawn(&gameState);
             },
         }
+        connection.status(&gameState);
     }
 }
--- client/src/menu/main.zig
+++ client/src/menu/main.zig
@@ -1,30 +1,58 @@
-const rl = @import("raylib");
+const assets = @import("../assets.zig");
 const config = @import("../config.zig");
+const rl = @import("raylib");
+const std = @import("std");
 const Button = @import("../components/button.zig");
 const Clickable = Button.Clickable{};
 const GameState = @import("../game/state.zig");
 
 pub const Menu = struct {
-    pub const Login = struct {
+    pub const Server = struct {
+        pub const bufferSize = 50;
+        address: [bufferSize:0]u8 = .{0} ** bufferSize,
+        addressPosition: usize = 0,
+        connectionStatus: bool = false,
+    };
+    pub const Credentials = struct {
+        pub const bufferSize = 50;
         username: [bufferSize:0]u8 = .{0} ** bufferSize,
         usernamePosition: usize = 0,
         password: [bufferSize:0]u8 = .{0} ** bufferSize,
         passwordPosition: usize = 0,
-        pub const bufferSize = 50;
+        email: [:0]const u8 = "",
+        login_button: Button.Clickable = Button.Clickable{ .disabled = true },
     };
     pub const Configuration = struct {
-        pub const ScreenMode = enum {
+        currentScreenMode: enum {
             windowed,
             fullscreen,
-        };
-        currentScreenMode: ScreenMode = .windowed,
+        } = .windowed,
     };
-    character_name: [:0]u8,
-    character_buttons: Button.SelectableGroup = .{},
-    join_world_button: Button.Clickable = Button.Clickable{ .disabled = true },
-    login: Login = .{},
-    email: [:0]const u8 = "",
+    pub const Assets = struct {
+        connection: struct {
+            connected_icon: rl.Texture2D,
+            not_connected_icon: rl.Texture2D,
+        },
+    };
+    pub const Character = struct {
+        pub const Creation = struct {
+            name_position: usize = 0,
+            name: [:0]u8,
+        };
+        pub const Selection = struct {
+            list: []const GameState.World.Character = &.{},
+            buttons: Button.SelectableGroup = .{},
+            join_world_button: Button.Clickable = Button.Clickable{ .disabled = true },
+        };
+        create: Creation,
+        select: Selection = .{},
+    };
+
+    credentials: Credentials = .{},
+    server: Server = .{},
     config: Configuration = .{},
+    character: Character,
+    assets: Assets,
 };
 
 fn userRegistryButton(gameState: *GameState) void {
@@ -49,7 +77,9 @@ fn userLoginButton(gameState: *GameState) void {
         .x = createUserButtonX,
         .y = createUserButtonY + buttonSize.y + config.menuButtonsPadding,
     };
-    if (Clickable.at(
+    const loginButton = &gameState.menu.credentials.login_button;
+    loginButton.disabled = !gameState.connection.is_connected;
+    if (loginButton.at(
         "Login",
         buttonPosition,
         buttonSize,
@@ -57,7 +87,35 @@ fn userLoginButton(gameState: *GameState) void {
     )) gameState.scene = .user_login;
 }
 
+fn userConnectButton(gameState: *GameState) void {
+    const buttonSize = Button.Sizes.extraLarge(gameState);
+    const createUserButtonX = (gameState.width / 2) - (buttonSize.x / 2);
+    const createUserButtonY = (gameState.height / 2) - (buttonSize.y / 2);
+    const buttonPosition: rl.Vector2 = .{
+        .x = createUserButtonX,
+        .y = createUserButtonY + 2 * buttonSize.y + 2 * config.menuButtonsPadding,
+    };
+    if (Clickable.at(
+        "Connect",
+        buttonPosition,
+        buttonSize,
+        config.ColorPalette.primary,
+    )) gameState.scene = .connect;
+}
+
 pub fn spawn(gameState: *GameState) void {
     userRegistryButton(gameState);
     userLoginButton(gameState);
+    userConnectButton(gameState);
+}
+
+pub fn loadAssets() !Menu.Assets {
+    const notConnected = try assets.texture("connected.png");
+    const connected = try assets.texture("not-connected.png");
+    return .{
+        .connection = .{
+            .not_connected_icon = notConnected,
+            .connected_icon = connected,
+        },
+    };
 }
