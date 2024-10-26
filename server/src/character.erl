74ed4d244bdcf73b7752d661b761678fdd4feee1
EduardoLR10 <dudulr10@gmail.com>
2024-10-26 01:02:26 -0300

    Make server resilient to fails in PSQL functions

--- server/src/character.erl
+++ server/src/character.erl
@@ -13,7 +13,9 @@ create(#{name := Name,
 	 faith := Faith}, Connection) ->
     Query = "INSERT INTO character.view (name,  e_mail, username, constitution, wisdom, strength, endurance, intelligence, faith) \
              VALUES ($1::VARCHAR(18), $2::TEXT, $3::VARCHAR(32), $4::SMALLINT, $5::SMALLINT, $6::SMALLINT, $7::SMALLINT, $8::SMALLINT, $9::SMALLINT)",
-    {ok, _} = epgsql:equery(Connection, Query, [Name, Username, Email, Constitution, Wisdom, Strength, Endurance, Intelligence, Faith]).
+    Result = epgsql:equery(Connection, Query, [Name, Username, Email, Constitution, Wisdom, Strength, Endurance, Intelligence, Faith]),
+    Fun = (fun (_, _, _) -> ok end),
+    util:process_postgres_result(Result, insert, Fun).
 
 %% TODO: Add a Select first in order to check already being activated
 activate(#{name := Name, 
@@ -24,15 +26,17 @@ activate(#{name := Name,
              VALUES ($1::VARCHAR(18), $2::TEXT, $3::VARCHAR(32), $4::VARCHAR(50)) \
              ON CONFLICT (name, username, e_mail) \
              DO UPDATE SET user_pid = $4::VARCHAR(50)",
-    {ok, _} = epgsql:equery(Connection, Query, [Name, Email, Username, UserPid]),
-    ok.
+    Result = epgsql:equery(Connection, Query, [Name, Email, Username, UserPid]),
+    Fun = (fun (_, _, _) -> ok end),
+    util:process_postgres_result(Result, insert, Fun).
 
 %% TODO: Add a Select first in order to check already being deactivated
 deactivate(UserPid, Connection) ->
     Query = "DELETE FROM character.active \ 
              WHERE user_pid = $1::VARCHAR(50)",
-    {ok, _} = epgsql:equery(Connection, Query, [UserPid]),
-    ok.
+    Result = epgsql:equery(Connection, Query, [UserPid]),
+    Fun = (fun (_, _, _) -> ok end),
+    util:process_postgres_result(Result, insert, Fun).
 
 update(#{name := Name, 
 	 username := Username, 
@@ -49,11 +53,12 @@ update(#{name := Name,
              x_velocity = $3::REAL, y_velocity = $4::REAL, \
              face_direction = $5::SMALLINT, state_type = $6::STATE_TYPE \ 
              WHERE name = $7::VARCHAR(18) AND e_mail = $8::TEXT AND username = $9::VARCHAR(32) AND map_name = $10::VARCHAR(64)",
-    {ok, _} = epgsql:with_transaction(Connection, 
+    Result = epgsql:with_transaction(Connection, 
 				      fun (Conn) -> epgsql:equery(Conn, Query, [XPosition, YPosition, XVelocity, YVelocity, FaceDirection, StateType, Name, Email, Username, MapName])
 				      end,
 				      #{ begin_opts => "ISOLATION LEVEL READ UNCOMMITTED"}),
-    ok = epgsql:sync(Connection).
+    Fun = (fun (_) -> epgsql:sync(Connection) end),
+    util:process_postgres_result(Result, update, Fun).
 
 
 retrieve_near_players(#{map_name := MapName}, UserPid, Connection) ->
@@ -74,8 +79,10 @@ retrieve_near_players(#{map_name := MapName}, UserPid, Connection) ->
              FROM character.view \
              NATURAL JOIN character.active \
              WHERE map_name = $1::VARCHAR(64) AND user_pid <> $2::VARCHAR(50)",
-    {ok, FullColumns, Values} = epgsql:equery(Connection, Query, [MapName, UserPid]),
-    util:transform_character_map(util:columns_and_rows(FullColumns, Values)).
+    Result = epgsql:equery(Connection, Query, [MapName, UserPid]),
+    Fun = (fun (FullColumns, Values) -> {ok, util:transform_character_map(util:columns_and_rows(FullColumns, Values))} end),
+    util:process_postgres_result(Result, select, Fun).
+
 
 player_characters(Username, Email, Connection) ->
     Query = "SELECT character.view.name, \
@@ -93,7 +100,8 @@ player_characters(Username, Email, Connection) ->
                     character.view.face_direction, \
                     character.view.state_type \
              FROM character.view WHERE username = $1::VARCHAR(32) AND e_mail = $2::TEXT",
-    {ok, FullColumns, Values} = epgsql:equery(Connection, Query, [Username, Email]),
-    util:transform_character_map(util:columns_and_rows(FullColumns, Values)).
+    Result = epgsql:equery(Connection, Query, [Username, Email]),
+    Fun = (fun (FullColumns, Values) -> {ok, util:transform_character_map(util:columns_and_rows(FullColumns, Values))} end),
+    util:process_postgres_result(Result, select, Fun).
     %% io:format("Username: ~p, Email: ~p, Data: ~p\n", [Username, Email, Something]),    
 
--- server/src/util.erl
+++ server/src/util.erl
@@ -1,33 +1,63 @@
 -module(util).
 
--export([columns_and_rows/2, transform_character_map/1]).
+-export([columns_and_rows/2, transform_character_map/1, process_postgres_result/3]).
 
 column_names(Columns) ->
     lists:map(
         fun({_, Name, Type, _, _, _, _, _, _}) -> {Type, erlang:binary_to_atom(Name)} end, Columns
     ).
 
-columns_and_rows(FullColumns, Rows) ->
-    Columns = column_names(FullColumns),
-    F =
-        (fun(Row) ->
-            lists:zipwith(
-                fun({Type, ColumnName}, RowValue) ->
-                    Value =
-                        case Type of
-                            varchar -> erlang:binary_to_list(RowValue);
-                            text -> erlang:binary_to_list(RowValue);
-                            _ -> RowValue
-                        end,
-                    {ColumnName, Value}
-                end,
-                Columns,
-                erlang:tuple_to_list(Row)
-            )
-        end),
-    Values = lists:map(F, Rows),
-    lists:map(fun maps:from_list/1, Values).
+columns_and_rows(FullColumns, MaybeRows) ->
+    case MaybeRows of
+	[] -> [];
+	Rows ->
+	    io:format("FullColumns: ~p, Rows: ~p\n", [FullColumns, Rows]),
+	    Columns = column_names(FullColumns),
+	    F =
+		(fun(Row) ->
+			 lists:zipwith(
+			   fun({Type, ColumnName}, RowValue) ->
+				   Value =
+				       case Type of
+					   varchar -> erlang:binary_to_list(RowValue);
+					   text -> erlang:binary_to_list(RowValue);
+					   _ -> RowValue
+				       end,
+				   {ColumnName, Value}
+			   end,
+			   Columns,
+			   erlang:tuple_to_list(Row)
+			  )
+		 end),
+	    Values = lists:map(F, Rows),
+	    lists:map(fun maps:from_list/1, Values)
+    end.
 
 transform_character_map(List) ->
     F = (fun(Map) -> Map#{state_type := erlang:binary_to_atom(maps:get(state_type, Map))} end),
     lists:map(F, List).
+
+process_postgres_result(Result, Tag, Fun) ->
+    case Tag of
+	select -> case Result of
+		      {ok, FullColumns, Values} -> Fun(FullColumns, Values);
+		      _ -> {error, "Unexpected result from Select"}
+		  end;
+	update -> case Result of
+		      {ok, Count} -> Fun(Count);
+		      _ -> {error, "Unexpected result from Update"}
+		  end;
+	delete -> case Result of
+		      {ok, Count} -> Fun(Count);
+		      _ -> {error, "Unexpected result from Delete"}
+		  end;
+	insert -> case Result of
+		      {ok, Count, Columns, Rows} -> Fun(Count, Columns, Rows);
+		      _ -> {error, "Unexpected result from Insert"}
+		  end;
+	_ -> {error, "Unrecognized operation in Server side"}
+    end.
+			  
+			   
+
+		    
