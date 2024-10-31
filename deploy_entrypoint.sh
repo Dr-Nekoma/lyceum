f198b5d603119d98534a435f967179ce334f162e
Marcos Benevides <marcos.schonfinkel@gmail.com>
2024-10-31 20:39:14 -0300

    ci: fix deploy and add benin as a player

--- deploy_entrypoint.sh
+++ deploy_entrypoint.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
 
-export ERL_DIST_PORT=5000
-
-# AAAAAAAAAAAAA, forgot that I have to build this again...
-nix build .#server
-
-if [[ $(./result/bin/server ping) == "pong" ]]; then
-    echo "Restarting app..."
-    ./result/bin/server restart -detached
-else
-    echo "Starting app..."
-    ./result/bin/server foreground -detached
-fi
-
+# Luckily, we've already cached the build from CI
+sudo nix build .#dockerImage
+sudo docker load < result
+sudo docker compose up -d
