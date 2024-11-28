bcb460109312d8e7403c35e725a088e3a1c92788
EduardoLR10 <dudulr10@gmail.com>
2024-11-28 04:28:33 -0300

    Add erlando to main's project config

--- server/rebar-deps.nix
+++ server/rebar-deps.nix
@@ -24,6 +24,16 @@ let
       };
       beamDeps = [ ];
     };
+    erlando = builder {
+      name = "erlando";
+      version = "3.1.1";
+      src = fetchHex {
+        pkg = "erlando";
+        version = "3.1.1";
+        sha256 = "sha256-ljPLSXjb14gn+nbqTDsL0kz71N2a9ICI6SLnEVSUo0Q=";
+      };
+      beamDeps = [ ];
+    };
     epgsql = builder {
       name = "epgsql";
       version = "4.7.1";
@@ -42,7 +52,7 @@ let
         version = "3.0.3";
         sha256 = "sha256-dB0aVaq62qPg/hMFEFAQGnPpDEVwuflAOpOdlUaBNSE=";
       };
-      beamDeps = [ pc ];
+      beamDeps = [ ];
     };
   };
 in self
