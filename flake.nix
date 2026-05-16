a4b66d6ddce68dfbea4410d84e5f4d592fcf2a1d
Marcos Benevides <marcos.schonfinkel@gmail.com>
2026-05-16 08:33:45 -0400

    refactor: improve nix setup to properly leverage flake-parts (#168)

--- flake.nix
+++ flake.nix
@@ -15,113 +15,33 @@
   };
 
   outputs =
-    {
+    inputs@{
       self,
       nixpkgs,
-      devenv,
       flake-parts,
-      treefmt-nix,
       ...
-    }@inputs:
+    }:
     flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+        inputs.treefmt-nix.flakeModule
+        ./client/flake-module.nix
+        ./server/flake-module.nix
+      ];
+
       systems = [
         "x86_64-linux"
         "aarch64-linux"
         "aarch64-darwin"
-        "x86_64-darwin"
       ];
+
       perSystem =
-        { pkgs, system, ... }:
-        let
-          # Environment-specific packages
-          linuxPkgs = with pkgs; [
-            inotify-tools
-            glfw
-            libGL
-            libpulseaudio
-            libxkbcommon
-            pkg-config
-            libxcb
-            libxft
-            libx11
-            libx11.dev
-            libxrandr
-            libxinerama
-            libxcursor
-            libxi
-            # Wayland stuff
-            wayland
-            wayland-protocols
-            wayland-scanner
-          ];
-          linuxLibs =
-            with pkgs;
-            lib.makeLibraryPath [
-              libGL
-              libxkbcommon
-              liburing
-              raylib
-              libxcb
-              libxft
-              libx11
-              libx11.dev
-              libxrandr
-              libxinerama
-              libxcursor
-              libxi
-              # Wayland stuff
-              glfw
-              wayland
-              wayland-protocols
-            ];
-          darwinPkgs = with pkgs.darwin.apple_sdk.frameworks; [
-            CoreFoundation
-            CoreServices
-          ];
-
-          devPackages =
-            with pkgs;
-            [
-              erlang-language-platform
-              erlfmt
-              just
-              postgresql
-              raylib
-              sqls
-              watchman
-            ]
-            ++ lib.optionals stdenv.isLinux linuxPkgs
-            ++ lib.optionals stdenv.isDarwin darwinPkgs;
-
-          # App config
-          app_name = "lyceum";
-          app_version = "0.2.3";
-
-          # Erlang
-          erlangVersion = pkgs.erlang;
-          erl_app = "server";
-
-          # Zig
-          zig_app = "lyceum-client";
-          zigVersion = pkgs.zig_0_15;
-          raylib = pkgs.raylib;
-
-          mkEnvVars = pkgs: erl: raylib: {
-            LOCALE_ARCHIVE = pkgs.lib.optionalString pkgs.stdenv.isLinux "${pkgs.glibcLocales}/lib/locale/locale-archive";
-            LANG = "en_US.UTF-8";
-            # https://www.erlang.org/doc/man/kernel_app.html
-            ERL_AFLAGS = "-kernel shell_history enabled";
-            ERL_INCLUDE_PATH = "${erl}/lib/erlang/usr/include";
-            # Devenv sets this to something else
-            # https://www.postgresql.org/docs/7.0/libpq-envars.htm
-            PGHOST = "127.0.0.1";
-            PRE_COMMIT_ALLOW_NO_CONFIG = 1;
-            # Waylad setup
-            GLFW_SCALE_TO_MONITOR = "GLFW_TRUE";
-          };
-
-          treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
-        in
+        {
+          pkgs,
+          system,
+          lib,
+          ...
+        }:
         {
           # This sets `pkgs` to a nixpkgs with allowUnfree option set.
           _module.args.pkgs = import nixpkgs {
@@ -129,173 +49,69 @@
             config.allowUnfree = true;
           };
 
-          # nix build
-          packages = rec {
-            # devenv up
+          # Shared constants/versions consumed by client/ and server/
+          # via the `lyceum` module argument.
+          _module.args.lyceum = {
+            app_name = "lyceum";
+            app_version = "0.2.3";
+            erlangVersion = pkgs.erlang;
+            zigVersion = pkgs.zig_0_15;
+          };
+
+          # nix build (devenv stuff)
+          packages = {
             devenv-up = self.devShells.${system}.default.config.procfileScript;
-
-            # devenv test
             devenv-test = self.devShells.${system}.default.config.test;
-
-            # Leverages nix to build the erlang backend release
-            # nix build .#server
-            server =
-              let
-                deps = import ./server/rebar-deps.nix {
-                  inherit (pkgs) fetchHex fetchFromGitHub fetchgit;
-                  builder = pkgs.beamPackages.buildRebar3;
-                };
-              in
-              pkgs.beamPackages.rebar3Relx {
-                pname = erl_app;
-                version = app_version;
-                root = ./server;
-                src = pkgs.lib.cleanSource ./server;
-                releaseType = "release";
-                profile = "prod";
-                include = [
-                  "rebar.config"
-                ];
-                buildInputs = (
-                  with pkgs;
-                  [
-                    coreutils
-                    gawk
-                    gnugrep
-                    openssl
-                  ]
-                  ++ lib.optional stdenv.isLinux [
-                    liburing
-                  ]
-                );
-                beamDeps = builtins.attrValues deps;
-                buildPhase = ''
-                  runHook preBuild
-                  HOME=. DEBUG=1 rebar3 as prod release --relname ${app_name}
-                  runHook postBuild
-                '';
-              };
-
-            # nix build .#dockerImage
-            dockerImage = pkgs.dockerTools.buildLayeredImage {
-              name = erl_app;
-              tag = "latest";
-              created = "now";
-              # This will copy the compiled erlang release to the image
-              contents = [
-                server
-              ];
-              config = {
-                Volumes = {
-                  "/opt/${erl_app}/etc" = { };
-                  "/opt/${erl_app}/data" = { };
-                  "/opt/${erl_app}/log" = { };
-                };
-                WorkingDir = "/opt/${erl_app}";
-                Cmd = [
-                  "${server}/bin/${erl_app}"
-                  "foreground"
-                ];
-                Env = [
-                  "ERL_DIST_PORT=8080"
-                  "ERL_AFLAGS=\"-kernel shell_history enabled\""
-                  "NODE_NAME=${erl_app}"
-                ];
-                ExposedPorts = {
-                  "4369/tcp" = { };
-                  "4369/ucp" = { };
-                  "8080/tcp" = { };
-                  "8080/udp" = { };
-                };
-              };
-            };
-
-            # nix build .#client
-            client = pkgs.stdenv.mkDerivation {
-              pname = zig_app;
-              version = app_version;
-              src = pkgs.lib.cleanSource ./client;
-
-              zigBuildFlags = [
-                "-fsys=raylib"
-                "--release=fast"
-                "-Dassets=${builtins.toString ./client}/assets"
-              ];
-
-              nativeBuildInputs = [
-                zigVersion.hook
-                pkgs.makeWrapper
-              ];
-
-              buildInputs =
-                with pkgs;
-                [
-                  raylib
-                  erlangVersion
-                ]
-                ++ lib.optionals stdenv.isLinux (linuxPkgs)
-                ++ lib.optionals stdenv.isDarwin darwinPkgs;
-
-              # To re-generate the nix lockfile:
-              # just client-deps
-              postConfigure = ''
-                mkdir -p "$ZIG_GLOBAL_CACHE_DIR/p"
-                for pkg in ${pkgs.callPackage ./client/build.zig.zon.nix { zig = zigVersion; }}/*; do
-                  ln -s "$pkg" "$ZIG_GLOBAL_CACHE_DIR/p/$(basename "$pkg")"
-                done
-              '';
-
-              postInstall = ''
-                wrapProgram "$out/bin/${zig_app}" --prefix LD_LIBRARY_PATH ":" "${linuxLibs}"
-              '';
-            };
-
           };
 
-          # nix run
-          apps = {
+          # Bare minimum config needed for CI.
+          # nix develop .#ci
+          devShells.ci = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              erlang
+              just
+              rebar3
+              rsync
+              zig_0_15
+              raylib
+            ];
           };
 
-          devShells = {
-            # `nix develop .#ci`
-            # reduce the number of packages to the bare minimum needed for CI
-            ci = pkgs.mkShell {
-              env = mkEnvVars pkgs erlangVersion raylib;
-              buildInputs = with pkgs; [
-                erlangVersion
-                just
-                rebar3
-                rsync
-                zigVersion
-                raylib
-              ];
+          # Shared dev-shell wiring; client/ and server/ flake-modules
+          # extend this with their own languages, services, packages,
+          # env, and scripts.
+          # `nix develop --impure`
+          devenv.shells.default = {
+            packages = with pkgs; [
+              just
+            ];
+
+            env = {
+              LOCALE_ARCHIVE = lib.optionalString pkgs.stdenv.isLinux "${pkgs.glibcLocales}/lib/locale/locale-archive";
+              LANG = "en_US.UTF-8";
+              PRE_COMMIT_ALLOW_NO_CONFIG = 1;
             };
 
-            # `nix develop --impure`
-            default = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [
-                (import ./devshell.nix {
-                  inherit
-                    pkgs
-                    mkEnvVars
-                    erlangVersion
-                    zigVersion
-                    raylib
-                    app_name
-                    system
-                    ;
-                  packages = devPackages;
-                })
-              ];
-            };
+            scripts.build.exec = "just build";
+
+            enterShell = ''
+              echo "Starting Development Environment..."
+            '';
+
+            enterTest = ''
+              just test
+            '';
           };
 
-          # nix fmt
-          formatter = treefmtEval.config.build.wrapper;
+          # nix fmt + nix flake check (auto-wired by treefmt-nix flakeModule)
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.erlfmt.enable = true;
+            programs.nixfmt.enable = true;
+            programs.zig.enable = true;
+          };
         };
 
-      flake = {
-      };
+      flake = { };
     };
 }
---
+++ server/flake-module.nix
@@ -0,0 +1,212 @@
+{
+  perSystem =
+    {
+      pkgs,
+      lib,
+      lyceum,
+      self',
+      ...
+    }:
+    let
+      inherit (lyceum) app_name app_version erlangVersion;
+      erl_app = "server";
+
+      # Strip .nix files so edits to this module don't invalidate the
+      # server build cache.
+      src = lib.cleanSourceWith {
+        src = ./.;
+        filter =
+          name: type:
+          (lib.cleanSourceFilter name type) && !(lib.hasSuffix ".nix" (baseNameOf name));
+      };
+    in
+    {
+      # nix build .#server
+      packages.server =
+        let
+          deps = import ./rebar-deps.nix {
+            inherit (pkgs) fetchHex fetchFromGitHub fetchgit;
+            builder = pkgs.beamPackages.buildRebar3;
+          };
+        in
+        pkgs.beamPackages.rebar3Relx {
+          pname = erl_app;
+          version = app_version;
+          root = ./.;
+          inherit src;
+          releaseType = "release";
+          profile = "prod";
+          plugins = [
+            pkgs.beamPackages.pc
+          ];
+          include = [
+            "rebar.config"
+          ];
+          buildInputs = (
+            with pkgs;
+            [
+              coreutils
+              gawk
+              gnugrep
+              openssl
+            ]
+            ++ lib.optional stdenv.isLinux [
+              liburing
+            ]
+          );
+          beamDeps = builtins.attrValues deps;
+          buildPhase = ''
+            runHook preBuild
+            HOME=. DEBUG=1 rebar3 as prod release --relname ${app_name}
+            runHook postBuild
+          '';
+        };
+
+      # nix build .#dockerImage
+      packages.dockerImage = pkgs.dockerTools.buildLayeredImage {
+        name = erl_app;
+        tag = "latest";
+        created = "now";
+        contents = [
+          self'.packages.server
+        ];
+        config = {
+          Volumes = {
+            "/opt/${erl_app}/etc" = { };
+            "/opt/${erl_app}/data" = { };
+            "/opt/${erl_app}/log" = { };
+          };
+          WorkingDir = "/opt/${erl_app}";
+          Cmd = [
+            "${self'.packages.server}/bin/${erl_app}"
+            "foreground"
+          ];
+          Env = [
+            "ERL_DIST_PORT=8080"
+            "ERL_AFLAGS=\"-kernel shell_history enabled\""
+            "NODE_NAME=${erl_app}"
+          ];
+          ExposedPorts = {
+            "4369/tcp" = { };
+            "4369/ucp" = { };
+            "8080/tcp" = { };
+            "8080/udp" = { };
+          };
+        };
+      };
+
+      # nix run .#server -- foreground
+      apps.server = {
+        type = "app";
+        program = "${self'.packages.server}/bin/${erl_app}";
+      };
+
+      # Server-side dev-shell contributions, merged into the shared
+      # devenv shell defined at the top level.
+      devenv.shells.default = {
+        languages.erlang = {
+          enable = true;
+          package = erlangVersion;
+        };
+
+        packages = with pkgs; [
+          erlang-language-platform
+          erlfmt
+          postgresql
+          sqls
+          watchman
+        ];
+
+        env = {
+          # https://www.erlang.org/doc/man/kernel_app.html
+          ERL_AFLAGS = "-kernel shell_history enabled";
+          ERL_INCLUDE_PATH = "${erlangVersion}/lib/erlang/usr/include";
+          # Devenv sets this to something else
+          # https://www.postgresql.org/docs/7.0/libpq-envars.htm
+          PGHOST = "127.0.0.1";
+        };
+
+        scripts = {
+          server.exec = "just server";
+          shell.exec = "just shell";
+          db-up.exec = "just db-up";
+          db-down.exec = "just db-down";
+          db-reset.exec = "just db-reset";
+          pg-con.exec = "just db";
+        };
+
+        services.postgres = {
+          enable = true;
+          package = pkgs.postgresql_18;
+          extensions = ext: [
+            ext.periods
+            ext.omnigres
+          ];
+          initdbArgs = [
+            "--locale=C"
+            "--encoding=UTF8"
+          ];
+          settings = {
+            shared_preload_libraries = "pg_stat_statements";
+            session_preload_libraries = "auto_explain";
+            "auto_explain.log_min_duration" = 150;
+            "auto_explain.log_analyze" = true;
+            log_min_duration_statement = 0;
+            log_statement = "all";
+            # pg_stat_statements config, nested attr sets need to be
+            # converted to strings, otherwise postgresql.conf fails
+            # to be generated.
+            compute_query_id = "on";
+            "pg_stat_statements.max" = 10000;
+            "pg_stat_statements.track" = "all";
+            # Adjust shared buffers
+            shared_buffers = "1GB";
+            # Increase work memory for large operations
+            work_mem = "16MB";
+            # Enable huge pages if available
+            huge_pages = "try";
+            # Adjust I/O concurrency settings
+            effective_io_concurrency = 16;
+            maintenance_io_concurrency = 16;
+          }
+          // lib.optionalAttrs pkgs.stdenv.isLinux {
+            # Async IO, io_uring or workers
+            # For io_uring method (Linux only, requires liburing)
+            io_method = "io_uring";
+          }
+          // lib.optionalAttrs pkgs.stdenv.isDarwin {
+            # in case "io_uring" is not available
+            io_method = "worker";
+            # For systems with many CPU cores and high I/O latency
+            io_workers = 8;
+            # For smaller systems or fast local storage
+            # io_workers = 2;
+          };
+          initialDatabases = [
+            {
+              name = app_name;
+              user = app_name;
+              pass = app_name;
+              initialSQL = builtins.readFile ./database/local/init.sql;
+            }
+          ];
+          port = 5432;
+          listen_addresses = "127.0.0.1";
+          initialScript = ''
+            -- The app user
+            ALTER USER ${app_name} SUPERUSER CREATEROLE;
+
+            CREATE USER migrations
+            WITH LOGIN SUPERUSER CREATEROLE PASSWORD 'migrations';
+
+            CREATE USER admin
+            WITH LOGIN SUPERUSER CREATEROLE PASSWORD 'admin';
+
+            CREATE USER application WITH LOGIN PASSWORD 'application';
+            CREATE USER lyceum_auth WITH LOGIN PASSWORD 'lyceum_auth';
+            CREATE USER mnesia WITH LOGIN PASSWORD 'mnesia';
+          '';
+        };
+      };
+    };
+}
